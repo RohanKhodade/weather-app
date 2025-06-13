99168529d0bc1f7d8471c415da15218bec88a148
RohanKhodade <rohankhodade883@gmail.com>
2025-06-13 13:23:16 +0530

    end to end weather app completed

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{your api key here}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
